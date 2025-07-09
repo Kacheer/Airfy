019839411d35eb0da2bcc3663b86166b0fc28d73
Ernitos <ernestturkovski@gmail.com>
2025-07-09 17:32:21 +0300

    изменил CurrentForecast

--- airfy-app/src/components/CurrentForecast.vue
+++ airfy-app/src/components/CurrentForecast.vue
@@ -1,11 +1,14 @@
 <template>
 	<div class="forecast-container">
-		<p>Город: {{ city }}</p>
-		<p>Температура: {{ temperature }}°C</p>
-		<p>Состояние: {{ condition }}</p>
-		<p>Влажность: {{ humidity }}%</p>
+		<div class="weather-info">
+			<p class="city">Город: {{ city }}</p>
+			<p class="temperature">Температура: {{ temperature }}°C</p>
+			<p class="condition">Состояние: {{ condition }}</p>
+		</div>
+		<p class="now">Сейчас</p>
 	</div>
 </template>
+
 <script>
 export default {
 	name: 'CurrentForecast',
@@ -22,41 +25,65 @@ export default {
 			type: String,
 			required: true,
 		},
-		humidity: {
-			type: Number,
+		now: {
+			type: String,
 			required: true,
 		},
 	},
 }
 </script>
+
 <style scoped>
 .forecast-container {
 	background: rgba(0, 0, 0, 0.4);
 	border-radius: 12px;
-	padding: 1.5rem;
+	padding: 2rem;
 	color: white;
-	text-align: center;
-	max-width: 300px;
-	margin: auto;
+	max-width: 500px;
+	margin: 50px auto;
 	backdrop-filter: blur(6px);
+	position: relative;
+	text-align: center;
+	height: 400px;
 }
 
 .city {
+	position: absolute;
+	top: 20px;
+	left: 20px;
 	font-size: 1.4rem;
 	font-weight: bold;
 }
+.now {
+	top: 5px;
+	font-size: 2.2rem;
+	font-weight: bold;
+	color: white;
+}
 
-.icon {
-	margin-right: 0.5rem;
+.temperature {
+	position: absolute;
+	top: 130px;
+	left: 50%;
+	transform: translateX(-50%);
+	font-size: 2.5rem;
+	font-weight: bold;
 }
 
 .condition {
-	font-size: 1.1rem;
-	margin-bottom: 0.3rem;
+	position: absolute;
+	top: 300px;
+	left: 50%;
+	transform: translateX(-50%);
+	font-size: 1.2rem;
 }
 
 .humidity {
-	font-size: 0.9rem;
+	position: absolute;
+	top: 270px;
+	left: 50%;
+	transform: translateX(-50%);
+	font-size: 1rem;
 	opacity: 0.7;
 }
 </style>
