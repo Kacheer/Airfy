c2df19243f3c18a8cc30a300f1cf795c728cd3ba
Ernitos <ernestturkovski@gmail.com>
2025-07-31 11:12:39 +0300

    коммит

--- airfy-app/src/App.vue
+++ airfy-app/src/App.vue
@@ -68,8 +68,7 @@ export default {
 			current: {
 				temperature: null,
 
-				weather_code: null
-
+				weather_code: null,
 			},
 			daily: [],
 			isDataLoaded: false,
@@ -83,32 +82,32 @@ export default {
 		}
 		apiBrowser.getPos()
 		apiForecast.fetchForecast()
-
 	},
 	async mounted() {
-		console.log("Начало получения позиции");
+		console.log('Начало получения позиции')
 		try {
-			const userPos = await apiBrowser.getPos();
-			DataService.addPosToStore(userPos.lat, userPos.long);
-			console.log("Координаты получены и сохранены");
-			await apiForecast.fetchForecast();
-			this.currentCity = await apiLocation.getCityNameByStore();
-			console.log("Прогноз получен");
-			const data = DataService.getAllData();
-			this.daily = data.daily;
-			this.units = data.units;
-			this.current = data.current;
-			this.isDataLoaded = true;
-			console.log(`В РАЗМЕТКЕ ПОЛУЧЕНЫ: ${this.units} \n ${this.current}\n${this.daily[0].temperature}`);
-			console.log("Weather code:", this.current.weather_code);
-			console.log("Current data:", this.current);
-			console.log("Hourly data:", this.daily[0].hourly); // Добавлено для отладки
+			const userPos = await apiBrowser.getPos()
+			DataService.addPosToStore(userPos.lat, userPos.long)
+			console.log('Координаты получены и сохранены')
+			await apiForecast.fetchForecast()
+			this.currentCity = await apiLocation.getCityNameByStore()
+			console.log('Прогноз получен')
+			const data = DataService.getAllData()
+			this.daily = data.daily
+			this.units = data.units
+			this.current = data.current
+			this.isDataLoaded = true
+			console.log(
+				`В РАЗМЕТКЕ ПОЛУЧЕНЫ: ${this.units} \n ${this.current}\n${this.daily[0].temperature}`
+			)
+			console.log('Weather code:', this.current.weather_code)
+			console.log('Current data:', this.current)
+			console.log('Hourly data:', this.daily[0].hourly) // Добавлено для отладки
 		} catch (error) {
-			console.error("Ошибка при получении данных:", error);
-			this.currentCity = 'Moscow';
-			await apiForecast.fetchForecast();
-			this.isDataLoaded = true;
-
+			console.error('Ошибка при получении данных:', error)
+			this.currentCity = 'Moscow'
+			await apiForecast.fetchForecast()
+			this.isDataLoaded = true
 		}
 	},
 	methods: {
@@ -121,7 +120,7 @@ export default {
 			console.log('Store city:', this.store.currentCity)
 		},
 		updateLanguage(newLang) {
-			console.log("Updated language:", newLang); // Добавлено для отладки
+			console.log('Updated language:', newLang) // Добавлено для отладки
 			this.selectedLanguage = newLang
 		},
 		round(number) {
@@ -143,16 +142,16 @@ export default {
 		currentTranslations() {
 			return language[this.selectedLanguage] || language['Русский']
 		},
-translatedDailyCards() {
-    if (!this.daily || this.daily.length < 7) return [];
-    return this.daily.slice(1, 7).map(day => ({
-        time: day.time, // Unix-время для дня
-        temperature: this.round(day.temperature),
-        feelsLike: this.round(day.feelsLike), // Ощущаемая температура
-        weatherCode: day.weather_code,
-        language: this.selectedLanguage
-    }));
-},
+		translatedDailyCards() {
+			if (!this.daily || this.daily.length < 7) return []
+			return this.daily.slice(1, 7).map(day => ({
+				time: day.time, // Unix-время для дня
+				temperature: this.round(day.temperature),
+				feelsLike: this.round(day.feelsLike), // Ощущаемая температура
+				weatherCode: day.weather_code,
+				language: this.selectedLanguage,
+			}))
+		},
 		convertSunriseTime() {
 			return Convert.toTime(this.daily[0].sunrise)
 		},
@@ -160,7 +159,10 @@ translatedDailyCards() {
 			return Convert.toTime(this.daily[0].sunset)
 		},
 		convertVisibility() {
-			return Convert.toVisibilityDesc(this.daily[0].visibility, this.selectedLanguage)
+			return Convert.toVisibilityDesc(
+				this.daily[0].visibility,
+				this.selectedLanguage
+			)
 		},
 		convertMinPressure() {
 			return Convert.toMillimetersOfMercury(this.daily[0].pressure_min)
@@ -169,74 +171,73 @@ translatedDailyCards() {
 			return Convert.toMillimetersOfMercury(this.daily[0].pressure_max)
 		},
 		hourlyForecast() {
-			if (!this.daily[0]?.hourly) return [];
+			if (!this.daily[0]?.hourly) return []
 			return this.daily[0].hourly.map(hour => ({
 				time: hour.time,
 				temperature: this.round(hour.temperature),
 				feelsLike: this.round(hour.apparent_temperature),
-				weatherCode: hour.weather_code || 0
-			}));
-			console.log("[APP] Hourly forecast:", this.hourlyForecast);
-		}
+				weatherCode: hour.weather_code || 0,
+			}))
+			console.log('[APP] Hourly forecast:', this.hourlyForecast)
+		},
 	},
 }
 </script>
 
 <template>
-
-  <div class="main-container">
-    <Header
-      :city="currentCity"
-      :language="selectedLanguage"
-      :theme="'Темная'"
-      @update:language="updateLanguage"
-    />
-    <div v-if="isDataLoaded" class="forecast-row">
-      <CurrentForecast
-        :temperature="round(current.temperature)"
-        :now="now"
-        :language="selectedLanguage"
-        :weather-code="current.weather_code"
-      />
-      <CurrentForecastDetails 
-        :humidity="daily[0].humidity"
-        :precipitation_probability="daily[0].precipitation_probability"
-        :pressure_min="convertMinPressure"
-        :pressure_max="convertMaxPressure"
-        :wind_speed="daily[0].wind_speed"
-        :visibility="convertVisibility"
-        :sunrise="convertSunriseTime"
-        :sunset="convertSunsetTime"
-        :language="selectedLanguage"
-      />
-    </div>
-    <div class="hourly-scroll-container">
-      <HourlyCard
-        v-for="(hour, idx) in hourlyForecast"
-        :key="idx"
-        :time="hour.time"
-        :temperature="hour.temperature"
-        :feelsLike="hour.feelsLike"
-        :weatherCode="hour.weatherCode"
-        :language="selectedLanguage"
-      />
-    </div>
-    <h2 class="forecast-title">
-      {{ currentTranslations.DailyCard.forecastTitle }}
-    </h2>
-    <div class="dailyContainer">
-<DailyCard
-    v-for="(card, idx) in translatedDailyCards"
-    :key="idx"
-    :time="card.time"
-    :temperature="card.temperature"
-    :feels-like="card.feelsLike"
-    :weather-code="card.weatherCode"
-    :language="card.language"
-/>
-    </div>
-  </div>
-
+	<div class="main-container">
+		<Header
+			:city="currentCity"
+			:language="selectedLanguage"
+			:theme="'Темная'"
+			@update:language="updateLanguage"
+			@update:theme="updateTheme"
+		/>
+		<div v-if="isDataLoaded" class="forecast-row">
+			<CurrentForecast
+				:temperature="round(current.temperature)"
+				:now="now"
+				:language="selectedLanguage"
+				:weather-code="current.weather_code"
+			/>
+			<CurrentForecastDetails
+				:humidity="daily[0].humidity"
+				:precipitation_probability="daily[0].precipitation_probability"
+				:pressure_min="convertMinPressure"
+				:pressure_max="convertMaxPressure"
+				:wind_speed="daily[0].wind_speed"
+				:visibility="convertVisibility"
+				:sunrise="convertSunriseTime"
+				:sunset="convertSunsetTime"
+				:language="selectedLanguage"
+			/>
+		</div>
+		<div class="hourly-scroll-container">
+			<HourlyCard
+				v-for="(hour, idx) in hourlyForecast"
+				:key="idx"
+				:time="hour.time"
+				:temperature="hour.temperature"
+				:feelsLike="hour.feelsLike"
+				:weatherCode="hour.weatherCode"
+				:language="selectedLanguage"
+			/>
+		</div>
+		<h2 class="forecast-title">
+			{{ currentTranslations.DailyCard.forecastTitle }}
+		</h2>
+		<div class="dailyContainer">
+			<DailyCard
+				v-for="(card, idx) in translatedDailyCards"
+				:key="idx"
+				:title="card.title"
+				:temperature="card.temperature"
+				:feels-like="card.feelsLike"
+				:weather-code="card.weatherCode"
+				:translations="currentTranslations"
+			/>
+		</div>
+	</div>
 </template>
 
 <style scoped>
