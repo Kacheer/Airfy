38ada2861adcea8fb0936f51b60ed5d5eff3b955
Kacher <kacherdeveloper@gmail.com>
2025-12-29 21:30:43 +0500

    chore: configure GitHub Pages deployment

--- airfy-app/vite.config.js
+++ airfy-app/vite.config.js
@@ -3,10 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+    base: '/Airfy/',
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
 })
